729b536d7e4737ed0e7f037a6e531f68646225d5
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-08-30 17:57:04 -0300

    add- página de excluir animal foi concluída

---
+++ public/animais/excluir_animal.php
@@ -0,0 +1,13 @@
+<?php
+
+$id = $_GET ['id'];
+
+include '../../infra/conexao.php';
+
+$sql = "DELETE FROM pets WHERE id = $id";
+if ($conn->query($sql) === TRUE) {
+echo "Animal foi excluído";
+echo "<button type="button" onclick=\"window.location.href='../../index.php'\">Voltar</button>;"
+} else {
+    echo "Erro ao excluir o animal: ". $conn->error;
+}
